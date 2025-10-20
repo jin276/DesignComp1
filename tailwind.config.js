170b12e8588b055f2f53eeb3844f132fb462dd31
Hitoshi Nishimura <hitoshinishimura@HitoshinoMacBook-Air.local>
2025-10-20 14:59:52 +0900

    feat: Completed setup files for Vercel deployment (Tailwind v4)

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+MediaSourceHandle.exports = {
+  content: ["./*.html", "./**/*.html"],
+  theme: {
+    extend: {
+      fontfamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+
+  plugins: [],
+}
